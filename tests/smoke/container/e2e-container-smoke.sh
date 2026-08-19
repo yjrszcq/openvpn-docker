fe9f205decfe46c2322e8e565217e33ad18ce22d
yjrszcq <yjr217@qq.com>
2026-08-19 18:02:25 +0800

    fix(test): allow selecting iptables backend

--- tests/smoke/container/e2e-container-smoke.sh
+++ tests/smoke/container/e2e-container-smoke.sh
@@ -103,7 +103,7 @@ YAML
     --network "$network" \
     --cap-add NET_ADMIN \
     --device /dev/net/tun \
-    -e OVPN_IPTABLES_BIN=iptables-legacy \
+    -e OVPN_IPTABLES_BIN="${OVPN_IPTABLES_BIN:-iptables-legacy}" \
     -v "$data_dir:/etc/openvpn" \
     -v "$config_dir:/etc/ovpn-conf:ro" \
     "$IMAGE" >"$protocol_dir/server.id"
